6f68ed77ad8ee12c430105771ab7464705bb22de
gh2440 gh2440 <hgc123123@qq.com>
2024-05-22 15:56:07 +0800

    add jobinfo

---
+++ cgroups/devices.go
@@ -0,0 +1,31 @@
+package cgroups
+
+import (
+	"strconv"
+	"strings"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type devices string
+
+// GetUsagePerCPU returns the per-nanosecond CPU usage of each CPU indexed from
+// 0
+func (c devices) GetUsagePerGPU() ([]int, error) {
+	var usage []int
+	data, err := readFile(string(c), "cpuacct.usage_percpu")
+        fmt.Println("data, err := readFile(string(c), cpuacct.usage_percpu)......%v",string(c))
+	if err != nil {
+		return usage, err
+	}
+	for _, usageStr := range strings.Split(strings.TrimSpace(data), " ") {
+		usageInt, err := strconv.Atoi(strings.TrimSpace(usageStr))
+		if err != nil {
+			log.Errorf("unable to convert per-cpu usage to integer: %v", err)
+			return usage, err
+		}
+		usage = append(usage, usageInt)
+	}
+	return usage, nil
+}
